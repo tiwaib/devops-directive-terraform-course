e2cc17bdc71abc57642527dcf649fbb2ebe86543
tiwaib <tiwai@techsynnovate.com>
2025-07-13 16:31:38 -0500

    Update main.tf

--- 06-organization-and-modules/web-app/main.tf
+++ 06-organization-and-modules/web-app/main.tf
@@ -2,7 +2,7 @@ terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    bucket         = "mydevops-directive-tf-state"
     key            = "06-organization-and-modules/web-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
@@ -41,7 +41,7 @@ module "web_app_1" {
   domain           = "devopsdeployed.com"
   app_name         = "web-app-1"
   environment_name = "production"
-  instance_type    = "t2.micro"
+  instance_type    = "t3.micro"
   create_dns_zone  = true
   db_name          = "webapp1db"
   db_user          = "foo"
@@ -56,7 +56,7 @@ module "web_app_2" {
   domain           = "anotherdevopsdeployed.com"
   app_name         = "web-app-2"
   environment_name = "production"
-  instance_type    = "t2.micro"
+  instance_type    = "t3.micro"
   create_dns_zone  = true
   db_name          = "webapp2db"
   db_user          = "bar"
